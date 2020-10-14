b7f6a2992382761fcb5d15a4d77c90aa88c6ef3d
Alex Witt <awitt2399@utexas.edu>
2020-10-14 16:13:24 -0700

    fix install script

--- docs/setup_linux.sh
+++ docs/setup_linux.sh
@@ -12,7 +12,7 @@ fi
 
 sudo apt update && \
     sudo apt upgrade -y && \
-    sudo apt install python3-dev build-essential
+    sudo apt install curl python3-dev build-essential
 
 pushd $(mktemp -d)
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -63,5 +63,7 @@ popd
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
+sudo apt-get update -y && sudo apt-get install google-cloud-sdk
 gcloud init
+
+sudo ln -sf $(which python3) /usr/bin/python
