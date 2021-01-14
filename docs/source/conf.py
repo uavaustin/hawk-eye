d5dcec5a596325aec0d278a2b2107b5d6b14d3ec
alexwitt2399 <awitt2399@utexas.edu>
2021-01-13 22:22:46 -0600

    move to read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,9 @@ release = "0.0.2b0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
