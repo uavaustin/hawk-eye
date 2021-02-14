d55e0beaf72e65e6ded20a2efd1579449d7e7040
alexwitt23 <awitt2399@utexas.edu>
2021-02-14 14:02:50 -0600

    updates

--- experimental/alex/main.cpp
+++ experimental/alex/main.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    std::string image_path = "/home/alex/Desktop/projects/uav/hawk-eye/hawk_eye/data_generation/data/competition-targets-2019/image-001160.jpg";
+    std::string image_path = "/home/alex/Desktop/projects/uav/hawk-eye/hawk_eye/data_generation/assets/competition-2019/image-001001.jpg";
     cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
 
     return 0;
