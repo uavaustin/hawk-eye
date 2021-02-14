b4eb4ec6e5c06f5ab34a36b2683ad7732ac382c4
alexwitt23 <awitt2399@utexas.edu>
2021-02-14 15:05:54 -0600

    try again

--- hawk_eye/setup/setup_env.sh
+++ hawk_eye/setup/setup_env.sh
@@ -14,7 +14,7 @@ fi
 
 sudo apt update && \
     sudo apt upgrade -y && \
-    sudo apt install -y curl gzip python3-dev cmake build-essential ninja-build
+    sudo apt install -y curl gzip python3-dev cmake gcc g++ build-essential ninja-build
 
 pushd $(mktemp -d)
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
